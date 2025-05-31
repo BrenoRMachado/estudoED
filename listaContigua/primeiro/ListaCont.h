2de709e79f8bfb5cc8d856df87e67789ada4dad0
Breno Machado <brenomachado.3064@gmail.com>
2025-05-30 23:01:21 -0300

    Adiciona get e set e lista de métodos

--- listaContigua/primeiro/ListaCont.h
+++ listaContigua/primeiro/ListaCont.h
@@ -1,10 +1,18 @@
 class ListaCont{
-    
     private:
 
     public:
 
     ListaCont();
     ~ListaCont();
+    int get();
+    void set();
+
+    void insereFinal();
+    void removeFinal();
+    void insereK();
+    void removeK();
+    
+
 
 };
