c5de0151eabbf73c9cdb9ac3fdebf44646fec71f
Breno Machado <brenomachado.3064@gmail.com>
2025-06-01 16:37:00 -0300

    Cria um main de teste

--- Listaencadeada/Simplesmenteencadeada/primeiro/main.cpp
+++ Listaencadeada/Simplesmenteencadeada/primeiro/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include "ListaEncad.h"
 using namespace std;
 
 int main()
 {
-    
+    ListaEncad l;
+
+    int n, val;
+    cout << "Escreva quantos nós tu quer: " << endl;
+    cin >> n;
+    for ( int i = 0 ; i<n ; i++)
+    {
+        cout << "Escreva o valor do nó " <<endl;
+        cin >> val;
+        l.insereInicio(val);
+    }
+
+    ///l.imprime();
+
     return 0;
 }
