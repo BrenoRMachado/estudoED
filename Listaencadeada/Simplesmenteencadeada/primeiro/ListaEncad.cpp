e755cfed9ca3a5be069a74e653a50e09945a834c
Breno Machado <brenomachado.3064@gmail.com>
2025-06-01 14:54:29 -0300

    esvazia o ponteiro primeiro no construtor  e implementa InsereInicio

--- Listaencadeada/Simplesmenteencadeada/primeiro/ListaEncad.cpp
+++ Listaencadeada/Simplesmenteencadeada/primeiro/ListaEncad.cpp
@@ -4,8 +4,18 @@ using namespace std;
 
 ListaEncad :: ListaEncad()
 {
-
+    primeiro = NULL; //com a lista vazia, o primeiro não deve apontar para ninguém
 }
 
 ListaEncad :: ~ListaEncad()
 {}
+
+void ListaEncad :: insereInicio(int val)
+{
+    No *p = new No(); //cria um nó apontado por  p
+
+    p->setInfo(val); //preeenche a info do nó com o valor
+    p->setProx(primeiro);   //faz o ponteiro desse nó ser o primeiro
+
+    primeiro = p;   //o endereço  que o ponteiro primeiro aponta vai ser o endereço do nó (objeto)
+}
