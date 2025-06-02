c7147789e0809e859eb7e8c1a46c0475e4913992
Breno Machado <brenomachado.3064@gmail.com>
2025-06-02 16:10:51 -0300

    Cria Cabeçalho da lista

--- 02 - Exercitando/ListaContigua/ListaCont.h
+++ 02 - Exercitando/ListaContigua/ListaCont.h
@@ -1,6 +1,20 @@
 class ListaCont{
     private:
+    int max; //valor máximo para tamanho  da lista
+    int n; //quantidade de n de nós
+    int *vet; //ponteiro que vou usar para alocar um vetor inteiro dinâmico 
 
     public:
+    ListaCont(int val);
+    ~ListaCont();
+
+    int getNo(int k); //indice k para identificar qual nó
+    void setNo(int k); //indice k para identificar qual nó
+    
+    void insereFinal(int val);
+    void removeFinal();
+
+    void insereK(int k, int val);
+    void removeK(int k);
     
 };
