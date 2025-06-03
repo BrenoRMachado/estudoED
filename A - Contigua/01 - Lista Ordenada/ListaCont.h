3fcdcad25c374b202b186f34483e68e6b26ecef9
Breno Machado <brenomachado.3064@gmail.com>
2025-06-03 20:33:16 -0300

    Altera métodos do ListaCont.h

--- A - Contigua/01 - Lista Ordenada/ListaCont.h
+++ A - Contigua/01 - Lista Ordenada/ListaCont.h
@@ -4,21 +4,26 @@ class ListaCont{
     int n; 
     int *vet;
     
+    int buscaBinaria(int val); //Adiciona um método privado que vamos usar para achar valores(não usaremos indices)
 
     public:
     ListaCont(int val); 
     ~ListaCont(); 
 
     int getNo(int k); 
-    void setNo(int k, int val); 
+    //void setNo(int k, int val);  //Não temos mais o set
     
-    void insereFinal(int val);
-    void removeFinal();
+    void insere(int val); //agora só temos 1 método para inserir. Ele insere de forma a manter a fila ordenada
+    void remove(); //Agora só temos 1 método para remover. Ele remove de forma a manter a fila ordenada
 
-    void insereK(int k, int val);
-    void removeK(int k);
+    //void insereK(int k, int val);
+    //void removeK(int k);
 
     void imprimirLista();
     int numeroNos(); 
+
+    // TODO: Novo método:
+    
+    bool busca(int val); //vai utilizar a busca binária para encontrar o índice do valor desejado
 
 };
