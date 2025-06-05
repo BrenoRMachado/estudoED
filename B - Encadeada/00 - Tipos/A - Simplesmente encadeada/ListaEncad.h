84ae00a8c2437930382f731627028e294b81b0c2
Breno Machado <brenomachado.3064@gmail.com>
2025-06-05 20:56:42 -0300

    Implementa novamente a simplesmente encadeada que sumiu

---
+++ B - Encadeada/00 - Tipos/A - Simplesmente encadeada/ListaEncad.h
@@ -0,0 +1,13 @@
+#include "No.h"
+class ListaEncad{
+    private:
+    No *primeiro; 
+
+    public:
+    ListaEncad();
+    ~ListaEncad();
+
+    void insereInicio(int val);
+    bool busca(int val);
+    void removeInicio(int val);
+};
