d905013f414468312da18bffe47fde61a9da4596
Breno <brenomachado.3064@gmail.com>
2025-06-29 17:28:51 -0300

    Implementa NoArv

---
+++ Segunda Prova/ArvoreBinaria01/NoArv.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "NoArv.h"
+using namespace std;
+
+NoArv :: NoArv()
+{
+    info = 0;
+    esq = nullptr;
+    dir = nullptr;
+}
+
+NoArv :: ~NoArv()
+{
+
+}
+
+int NoArv :: getInfo()
+{
+    return info;
+}
+
+NoArv* NoArv :: getEsq()
+{
+    return esq;
+}
+
+NoArv* NoArv :: getDir()
+{
+    return dir;
+}
+
+void NoArv :: setInfo(int val)
+{
+    info = val;
+}
+
+void NoArv :: setEsq(NoArv* p)
+{
+    esq = p;
+}
+void NoArv :: setDir(NoArv* p)
+{
+    dir = p;
+}
