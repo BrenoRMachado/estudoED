edebb062243ddde1ce7f9d9f125622826388c834
Breno <brenomachado.3064@gmail.com>
2025-06-29 17:56:45 -0300

    Cria classe ArvBin

---
+++ Segunda Prova/ArvoreBinaria01/ArvBin.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "ArvBin.h"
+
+ArvBin :: ArvBin()
+{
+    raiz = nullptr;
+}
+
+ArvBin :: ~ArvBin()
+{
+
+}
+
+int ArvBin :: getRaiz()
+{
+    return raiz->getInfo();
+}
+
+void ArvBin :: cria(int val, ArvBin *sae, ArvBin *sad)
+{
+
+}
+
+bool ArvBin :: vazia()
+{
+
+}
+
+void ArvBin :: imprime()
+{
+
+}
+
+bool ArvBin :: busca(int val)
+{
+    
+}
